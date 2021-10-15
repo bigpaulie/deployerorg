cbf3e658d72bad40eea4c8c5a9f8698a504cf514
Paul <bigpaulie25ro@yahoo.com>
2021-10-15 14:24:41 +0300

    Deployer test

--- deploy.php
+++ deploy.php
@@ -4,25 +4,35 @@ namespace Deployer;
 require 'recipe/laravel.php';
 
 // Project name
-set('application', 'my_project');
+set('application', 'deployerorg');
 
 // Project repository
-set('repository', 'git@domain.com:username/repository.git');
+set('repository', 'git@github.com:bigpaulie/deployerorg.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true); 
 
 // Shared files/dirs between deploys 
-add('shared_files', []);
-add('shared_dirs', []);
+add('shared_files', ['.env']);
+add('shared_dirs', ['storage']);
 
 // Writable dirs by web server 
-add('writable_dirs', []);
+add('writable_dirs', [
+    'bootstrap/cache',
+    'storage',
+    'storage/app',
+    'storage/app/public',
+    'storage/framework',
+    'storage/framework/cache',
+    'storage/framework/sessions',
+    'storage/framework/views',
+    'storage/logs',
+]);
 
 
 // Hosts
 
-host('project.com')
+host('ec2-3-127-142-220.eu-central-1.compute.amazonaws.com')
     ->set('deploy_path', '~/{{application}}');    
     
 // Tasks
@@ -36,5 +46,5 @@ after('deploy:failed', 'deploy:unlock');
 
 // Migrate database before symlink new release.
 
-before('deploy:symlink', 'artisan:migrate');
+//before('deploy:symlink', 'artisan:migrate');
 
